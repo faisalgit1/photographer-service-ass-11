3e8d91fb0c28ac848997603b2aa48ff08a07db5c
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-08 19:09:07 +0600

    routes-set

---
+++ src/Routes/Routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+import Login from '../components/Pages/Login'
+import Register from '../components/Pages/Register'
+
+import Main from '../layout/Main';
+
+
+const routes = createBrowserRouter([{
+    path: '/',
+    element: <Main></Main>,
+    children: [
+
+        {
+            path: '/login',
+            element: <Login></Login>
+        },
+        {
+            path: '/register',
+            element: <Register></Register>
+        }
+    ]
+}])
+
+
+export default routes;
+
